d9cc2c1fa95f3627fa0493599ee28b5590624179
Lukas Korbitsch <korbitschl@gmail.com>
2025-03-27 16:01:43 +0100

    contact js

---
+++ resources/js/contact.js
@@ -0,0 +1,14 @@
+export default {
+  firstName: 'Annika',
+  lastName: 'Korbitsch',
+  org: "Künstlerin",
+  email: 'annika.korbitsch' + '@' + 'gmail' + '.' + 'com',
+  phone: '+43' + '699' + '11986263',
+  phoneText: '+43' + ' 699' + ' 119' + ' 86' + ' 2' + ' 63',
+  webshop: 'https://www.cuirma.at',
+  webshop_short: 'cuirma.at',
+  address: 'Moorhofstraße 6',
+  city: 'Reichenfels',
+  zipCode: 9463,
+  country: 'Österreich'
+};
