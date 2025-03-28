61874d6786ecc7a51a659adb7d30573f20d488fc
Lukas Korbitsch <korbitschl@gmail.com>
2025-03-28 13:50:01 +0100

    Added deployment script

---
+++ deploy.php
@@ -0,0 +1,38 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/Korbe/annika-korbitsch-webapp.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+host('217.154.67.144')
+    ->set('ssh_multiplexing', false)
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '/var/www/annika.korbitsch.at');
+
+// Hooks
+task('npm:install', function () {
+    cd('{{release_path}}');
+    run('npm install');
+});
+
+task('npm:build', function () {
+    cd('{{release_path}}');
+    run('npm run build');
+});
+
+task('fpm:restart', function () {
+    run('systemctl restart php8.3-fpm');
+});
+
+after('deploy:vendors', 'npm:install');
+after('npm:install', 'npm:build');
+
+after('deploy:setup', 'deploy:unlock');
+after('deploy:failed', 'deploy:unlock');
